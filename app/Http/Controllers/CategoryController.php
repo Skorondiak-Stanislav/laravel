abe66244e1d2148a9e4bc8297b19d41311b7ca50
Skorondiak-Stanislav <skorondakstanislav@gmail.com>
2022-10-18 13:39:55 +0300

    feat: update category CRUD

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,34 +2,45 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use Illuminate\Http\JsonResponse;
+
 
 class CategoryController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
         return response()->json(Category::all());
-
-        
     }
-    public function store(StoreCategoryRequest $request)
+
+    public function store(StoreCategoryRequest $request): JsonResponse
     {
-        $data=$request->all();
-        
+        $data = $request->all();
+
         Category::create([
-            'name' =>$data['name'],
-            'descrition' =>$data['description'],
-            "type" =>$data["type"]
-        
+            'name' => $data['name'],
+            'description' => $data['description'],
+            'type' => $data['type'],
         ]);
-        
-        return Category::latest()->first()->get();
+
+        return response()->json(Category::latest()->first()->get());
     }
 
-    public function update(StoreCategoryRequest $request, Category $category)
+    public function update(UpdateCategoryRequest $request, Category $category): JsonResponse
     {
-        dd($category, $request->all());
+        $category->name = $request['name'];
+        $category->description = $request['description'];
+        $category->type = $request['type'];
+        
+        $category->save();
+
+        return response()->json($category, 201);
+    }
+
+    public function destroy(Category $category): JsonResponse
+    {
+        return response()->json($category->delete());
     }
 }
